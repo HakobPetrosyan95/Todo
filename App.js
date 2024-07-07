91485dd90bf1cdcdbbad9c75c88e72331481a9f8
HakobPetrosyan95 <152649422+HakobPetrosyan95@users.noreply.github.com>
2024-07-07 22:45:55 +0400

    Create App.js

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { TodoList } from './Components/TodoList'
+
+export  function App() {
+  return (
+    <div className='TodoList'>
+      <h1>Todo list</h1>
+      <TodoList/>
+    </div>
+  )
+}
